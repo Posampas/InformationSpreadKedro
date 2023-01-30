65200e8b39ddc2708c32224a4fa7843ef7991f5b
posampas <piotr.mierzejewski@mail.com>
2023-01-31 00:08:41 +0100

    stanza ner pipleline

--- src/informationspread/pipelines/named_entity_recognition/nodes.py
+++ src/informationspread/pipelines/named_entity_recognition/nodes.py
@@ -1,105 +1,32 @@
 import pandas as pd
-from ...processors.clarinService import ClarinService 
-from ...processors.xml_parser import XmlParser
 import stanza
 
-def remove_non_polish_locations(input: pd.DataFrame) -> pd.DataFrame:
-    return input[input["country"] == "Polska"]
 
-
-def extract_words_with_geo_assosiation_and_convert_it_to_base_form(twitts: pd.DataFrame) -> pd.DataFrame:
-    """ Returns base form of the words that have geo annotation.
-    Args : dataframe containg columns text and id
-
-    Retruns:
-        twitts: where in text are only presents the words that have geo annotation
-    """
-    _throw_if_column_not_present('text', twitts)
-    _throw_if_column_not_present('user_id', twitts)
-    lmpn = 'any2txt|wcrft2({"morfeusz2":false})|liner2({"model":"n82"})|ccl_emo({"lang":"polish"})'
-    total_len = len(twitts)
-    for i , row  in twitts.iterrows():
-        print(i + 1 , "/", total_len, 'user_id',row['user_id'])
-        text = row['text'][:20000]
-        baseFromService = ClarinService(text, lmpn)
-        response = baseFromService.run()
-        parsers = list(map(lambda x: XmlParser(x), response))
-        transformed = list(map(lambda x: x.extractBaseFormOfWordsThatHasGeoAnnotation(), parsers))
-        transformed = filter(lambda x : x, transformed) # remove resposenes that are empty
-        joined = ';'.join(transformed)
-        twitts.at[i , 'text'] = joined
-
-    return twitts
-
-def transform_place_names_to_geo_cordinates(twitts: pd.DataFrame) -> pd.DataFrame:
+def recognise_named_entites_stanza(twitts: pd.DataFrame) -> pd.DataFrame:
     """  
-    Args : dataframe containg columns text and id, where in text it expects to have been
-        provided with list string of Places containg names of places separated by semicolon ';'
+    Args : Dataframe containg colums user_id and text
+
+    It extract from text entites that can be recognised.
 
     Retruns:
-        twitts: 
-    """
-    _throw_if_column_not_present('user_id',twitts)
-    _throw_if_column_not_present('text',twitts)
-    users_geo_location_frames = []
-    for i, row in twitts.iterrows():
-        print(row['text'])
-        text = row['text'][:20000]
-        current_user_frame  = _get_cordinates_for_text(text)
-        current_user_frame['user_id'] = row['user_id']
-        users_geo_location_frames.append(current_user_frame)
-        
-    return pd.concat(users_geo_location_frames)
-
-
-def extarct_places_with_geo_location_stanza(twitts: pd.DataFrame) -> pd.DataFrame:
-    """  
-    Args : Dataframe containg colums id and text
-
-    It extract from text entites that can be recognised as geographical entites.
-
-    Retruns:
-        dataFrame containg two column id, and locations 
+        dataFrame containg columns user_id, entity and type 
     """ 
-    twitts['location'] = None
+    entity_col = 'entity'
+    entity_type_col = 'ent_type'
+    id_col = 'user_id'
     stanza.download('pl')
-    lemmatize = stanza.Pipeline(lang='pl', processors='tokenize,mwt,pos,lemma')
     nlp = stanza.Pipeline(lang='pl', processors='tokenize,mwt,pos,ner')
-
+    result_frame = pd.DataFrame()
     for id, row in twitts.iterrows():
-        print("input:", len(row['text']))
-        input_text = row['text']
-        lemma = lemmatize(input_text)
         doc = nlp(row['text'])
-        organizations = []
-        places  = [] 
+        entity = []
+        types  = [] 
         for ent in doc.ents:
-            if ent.type in  ('placeName', 'geogName' ):
-                places.append(ent.text)
-            elif ent.type == 'orgName':
-                organizations.append(ent.text) 
-            
-        lemmatize = stanza.Pipeline(lang='pl', processors='tokenize,mwt,pos,lemma')
-        doc = lemmatize(". ".join(places))
-        places_lemma = []
-        for word in doc.iter_words():
-            places_lemma.append(word.lemma)
-        twitts.at[id, 'location'] = ";".join(organizations + places_lemma)
-        print(twitts.at[id, 'location'])
-    return twitts.drop('text', axis='columns')
+            entity.append(ent.text)
+            types.append(ent.type) 
+    row_frame = pd.DataFrame({entity_col : entity , entity_type_col : types})
+    row_frame[id_col] = row[id_col]
+    result_frame = pd.concat([result_frame, row_frame], axis=0, ignore_index=True)
+        
+    return result_frame
     
-
-def _get_cordinates_for_text(text : str) :
-    lmpn = 'any2txt|wcrft2({"morfeusz2":false})|liner2({"model":"n82"})|geolocation({"limit":1})'
-    service = ClarinService(text=text,lmpn = lmpn)
-    response = service.run()
-    parsers = list(map(lambda x: XmlParser(x), response))
-    transformed = list(map(lambda x: x.extract_geo_addnotations(), parsers))
-    
-    return pd.concat(transformed)
-
-
-
-def _throw_if_column_not_present(column:str, twitts: pd.DataFrame) -> None:
-    if (column not in twitts.columns.to_list()):
-        raise RuntimeError("Column \"{}\" has to be present in the input frame".format(column)) 
--- src/informationspread/pipelines/named_entity_recognition/pipeline.py
+++ src/informationspread/pipelines/named_entity_recognition/pipeline.py
@@ -4,20 +4,20 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import remove_non_polish_locations, extract_words_with_geo_assosiation_and_convert_it_to_base_form, transform_place_names_to_geo_cordinates, extarct_places_with_geo_location_stanza
+from .nodes import recognise_named_entites_stanza
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=extarct_places_with_geo_location_stanza,
+                func=recognise_named_entites_stanza,
                 inputs="cleaned_data",
-                outputs="locations",
-                name="extract_words_with_geo_assosiations",
+                outputs="stanza_ner",
+                name="recognise_named_entites_with_stanza",
             ),
         ]
         ,
         namespace="ner_stanza",
         inputs="cleaned_data",
-        outputs="locations",
+        outputs="stanza_ner",
     )
